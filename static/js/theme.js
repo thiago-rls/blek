ef30aa9315ecc3f2b1a07e68ee18d1bbf01d077a
Thiago Rodrigues <thiagorodrigues@Thiagos-MacBook-Pro-ETS.local>
2026-03-18 21:51:33 -0300

    add main css

---
+++ static/js/theme.js
@@ -0,0 +1,9 @@
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.theme = 'light';
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.theme = 'dark';
+    }
+});
